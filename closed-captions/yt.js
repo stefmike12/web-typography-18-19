3ca1b3b5780c43b17e810bfaa2387f6743470b62
Mike van den Bos <stefmike12@gmail.com>
2019-05-20 17:26:12 +0200

    concept 1.0

--- closed-captions/yt.js
+++ closed-captions/yt.js
@@ -2,8 +2,8 @@ var player;
 
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('video-placeholder', {
-		width: 600,
-		height: 400,
+		width: 1200,
+		height: 800,
 		videoId: 'vrP-_T-h9YM',
 		playerVars: {
 			color: 'white'
